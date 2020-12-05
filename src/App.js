4f72d205933cab1be6fe361f53fe346e726cd68d
onok <onokje@gmail.com>
2020-12-05 20:58:50 +0100

    Added messages

--- src/App.js
+++ src/App.js
@@ -2,42 +2,59 @@ import React, {useEffect, useState} from "react";
 import Sheet from "./components/Sheet";
 import notes from './notes';
 import Mousetrap from 'mousetrap';
+import NotificationArea from "./components/NotificationArea";
 
 const getRandomNote = () => {
-    const randomI = Math.floor(Math.random() * Math.floor(notes.length))
+    const randomI = Math.floor(Math.random() * Math.floor(notes.length));
     return notes[randomI];
 }
 
 function App() {
-    const [currentNote, setCurrentNote] = useState(getRandomNote())
+    const [currentNote, setCurrentNote] = useState(getRandomNote());
+    const [msg, setMsg] = useState('');
+
+    const bindKeys = () => Mousetrap.bind(['a','b','c','d','e','f','g'], keyPress);
+    const unbindKeys = () => Mousetrap.unbind(['a','b','c','d','e','f','g']);
+
+    const reset = () => {
+        let randomNote = getRandomNote();
+        while (randomNote.name === currentNote.name) {
+            randomNote = getRandomNote();
+        }
+        setCurrentNote(randomNote);
+        setMsg('');
+    }
 
     const keyPress = (event) => {
+        console.log(event.key);
         if (currentNote.keyCode === event.key) {
-            console.log('Correct!');
-            reset();
+            setMsg('Correct!');
+            unbindKeys();
+            setTimeout(reset ,1000);
         } else {
-            console.log('Wrong!');
+            setMsg(`Sorry, it's not ${event.key}`);
         }
     }
 
-    const reset = () => {
-        setCurrentNote(getRandomNote());
+    useEffect(() => {
+        bindKeys();
+        return unbindKeys;
+    },[currentNote]);
+
+    if (currentNote) {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Notey
+                </header>
+                <Sheet note={currentNote}/>
+                <NotificationArea msg={msg} />
+            </div>
+        );
+    } else {
+        return <div>Loading...</div>;
     }
 
-    useEffect(() => {
-        Mousetrap.bind(['a','b','c','d','e','f','g'], keyPress)
-        return () => Mousetrap.unbind(['a','b','c','d','e','f','g']);
-    }, [currentNote]);
-
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                Notey
-            </header>
-            <Sheet note={currentNote}/>
-        </div>
-    );
 }
 
 export default App;
---
+++ src/components/NotificationArea.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotificationArea({msg}) {
+    return <div>{msg}</div>;
+}
